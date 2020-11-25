74cb3902acd3d5ad949969c7b7d9a0dcb0228bbf
FRANCISCO6547 <75024616+FRANCISCO6547@users.noreply.github.com>
2020-11-25 09:54:22 -0500

    Create AppCola.java
    Main

---
+++ AppCola.java
@@ -0,0 +1,16 @@
+public class AppCola{
+       public static void main (String args[]){
+              Cola c=new Cola();
+              c.poner("Maria");
+              c.poner("Sandra");
+              c.poner("Luis");
+              c.imprimir();
+              c.quitar();
+              c.imprimir();
+              c.quitar();
+              c.imprimir();
+              c.decolar();
+              c.imprimir();
+              System.out.println("-----");
+       }
+}
